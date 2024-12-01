703eafb41e87a9e565162ce4bd294bb4f347541e
Steve <165537257+SteveCasTo@users.noreply.github.com>
2024-11-30 23:30:40 -0400

    move method

--- logic.py
+++ logic.py
@@ -1,7 +1,23 @@
 import pygame
-
-def handle_events(board):
+from gadgets import SQUARE_SIZE as square_size
+def handle_events(board, selected_piece, turn):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            return False
-    return True
+            return False, selected_piece, turn  # Terminar el juego
+
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            x, y = pygame.mouse.get_pos()
+            row, col = y // square_size, x // square_size
+            piece = board.get_piece(row, col)
+            if piece and piece.color == turn:
+                selected_piece = piece
+
+        elif event.type == pygame.MOUSEBUTTONUP and selected_piece:
+            x, y = pygame.mouse.get_pos()
+            new_row, new_col = y // square_size, x // square_size
+            if board.move_piece(selected_piece, new_row, new_col):
+                # Cambiar turno después de un movimiento válido
+                turn = (0, 0, 0) if turn == (255, 255, 255) else (255, 255, 255)
+            selected_piece = None
+
+    return True, selected_piece, turn
--- runner.py
+++ runner.py
@@ -1,6 +1,5 @@
 import pygame
 import sys
-from gadgets import BLACK, SQUARE_SIZE as SQUARE_SIZE
 from logic import handle_events
 from drawer import Drawer
 
@@ -15,23 +14,7 @@ def game_run(screen, board):
         board.draw_pieces(drawer)
         pygame.display.flip()
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-                pygame.quit()
-                sys.exit()
-
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                x, y = pygame.mouse.get_pos()
-                row, col = y // SQUARE_SIZE, x // SQUARE_SIZE
-                piece = board.get_piece(row, col)
-                if piece and piece.color == turn:
-                    selected_piece = piece
-
-            elif event.type == pygame.MOUSEBUTTONUP and selected_piece:
-                x, y = pygame.mouse.get_pos()
-                new_row, new_col = y // SQUARE_SIZE, x // SQUARE_SIZE
-                if board.move_piece(selected_piece, new_row, new_col):
-                    # Cambiar turno después de un movimiento válido
-                    turn = (0, 0, 0) if turn == (255, 255, 255) else (255, 255, 255)
-                selected_piece = None
+        running, selected_piece, turn = handle_events(board, selected_piece, turn)
+        if not running:
+            pygame.quit()
+            sys.exit()
